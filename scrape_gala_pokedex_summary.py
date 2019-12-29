66cdcac58072d93728b92253ebf65f9f1b7b562e
Tim Adler <tim@emrys-merlin.de>
2019-12-30 00:47:53 +0100

    Other website is scraped
    The old website had multiple entries for Pokemon from older versions. This was a
    pain to deal with so I switched to another site only representing Galar Pokemon.
    This may lead to a small error (e.g. I only consider Galar-Mauzi, but not the
    standard Mauzi, although both can be obtained in the game).

--- scrape_gala_pokedex_summary.py
+++ scrape_gala_pokedex_summary.py
@@ -4,24 +4,24 @@ import pandas as pd
 from tqdm import tqdm
 
 g2e = {
-    'Normal': 'Normal',
-    "Kampf": 'Fight',
-    "Flug": 'Flying',
-    "Gift": 'Poison',
-    "Boden": 'Ground',
-    "Gestein": 'Rock',
-    "Käfer": 'Bug',
-    "Geist": 'Ghost',
-    "Stahl": 'Steel',
-    "Feuer": 'Fire',
-    "Wasser": 'Water',
-    "Pflanze": 'Grass',
-    "Elektro": 'Electric',
-    "Psycho": 'Psychic',
-    "Eis": 'Ice',
-    "Drache": 'Dragon',
-    "Unlicht": 'Dark',
-    "Fee": 'Fairy',
+    'normal': 'Normal',
+    "kampf": 'Fight',
+    "flug": 'Flying',
+    "gift": 'Poison',
+    "boden": 'Ground',
+    "gestein": 'Rock',
+    "kaefer": 'Bug',
+    "geist": 'Ghost',
+    "stahl": 'Steel',
+    "feuer": 'Fire',
+    "wasser": 'Water',
+    "pflanze": 'Grass',
+    "elektro": 'Electric',
+    "psycho": 'Psychic',
+    "eis": 'Ice',
+    "drache": 'Dragon',
+    "unlicht": 'Dark',
+    "fee": 'Fairy',
 }
 
 def scrape_pokewiki_table(url):
@@ -33,22 +33,27 @@ def scrape_pokewiki_table(url):
 
     for row in tqdm(rows):
         cells = row.find_all('td')
-        if len(cells) == 0:
-            continue
 
         ser = pd.Series()
         ser['id'] = int(cells[0].text)
-        ser['name'] = cells[2].text
+        ser['name'] = cells[1].text.strip()
 
-        ser['type'] = [g2e[l.get('title')] for l in cells[3].find_all('a')]
-        ser['n_type'] = len(ser['type'])
+        imgs = cells[2].find_all('img')
+        t0 = g2e[imgs[0].get('alt').lower()]
+        if len(imgs) == 2:
+            t1 = g2e[imgs[1].get('alt').lower()]
+            ser['type1'] = min(t0, t1)
+            ser['type2'] = max(t0, t1)
+        else:
+            ser['type1'] = t0
+        ser['n_type'] = len(imgs)
         res = res.append(ser, ignore_index=True)
 
     return res
 
 
 if __name__ == '__main__':
-    pokewiki_url = 'https://www.pokewiki.de/Liste_der_Pok%C3%A9mon_nach_Galar-Pok%C3%A9dex'
+    pokewiki_url = 'https://www.bisafans.de/spiele/editionen/schild-schwert/galar-dex.php'
 
     df = scrape_pokewiki_table(pokewiki_url)
 
